c7871b3df455d096c38c0fc78352b8c6399fb451
Corbin <94069924+Durpus@users.noreply.github.com>
2025-03-24 23:00:53 -0600

    Initial commit

---
+++ src/Experiment.java
@@ -0,0 +1,14 @@
+
+public class Experiment {
+    int number;
+    String name;
+    int weight;
+    int rating;
+
+    public Experiment(int number, String name, int weight, int rating) {
+        this.number = number;
+        this.name = name;
+        this.weight = weight;
+        this.rating = rating;
+    }
+}
